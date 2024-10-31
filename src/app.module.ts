a6a3a25b59643920cc5b400f3cd9ee41ee79b5bc
muyiwa <muyiwa100@yahoo.com>
2024-10-31 16:33:46 +0000

    fixes deply49

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { ConfigModule } from '@nestjs/config';
       
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT, 10) || 5432,
-      username: process.env.DB_USER || 'postgres',
+      username: process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD || 'pass123',
       database: process.env.DB_DATABASE || 'postgres',
       autoLoadEntities: true,
