734dd9cb11f5881614c274891bc6c3bc6dcda5f2
muyiwa <muyiwa100@yahoo.com>
2024-11-04 16:27:06 +0000

    fixes deply95

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'postgres',
       
-      host: process.env.DB_HOST || '192.168.49.2',
+      host: process.env.DB_HOST || '127.0.0.1',
       port: parseInt(process.env.DB_PORT, 10) || 5432,
       username: process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD || 'pass123',
